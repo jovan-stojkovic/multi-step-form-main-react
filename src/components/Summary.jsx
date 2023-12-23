b124db4c14a3ba41a59220027b32fa28cfdc6216
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2023-12-23 13:55:22 +0100

    Summary page

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,17 @@
 const Summary = () => {
-    return ( <>
-    <p>Thank you</p>
-    </> );
-}
- 
+  return (
+    <>
+      <div className="page-container summary-page-container">
+        <img src="https://www.iconbolt.com/iconsets/startup-business-line/checked.svg" alt="checked" />
+        <h1>Thank you!</h1>
+        <p>
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at support@loremgaming.com
+        </p>
+      </div>
+    </>
+  );
+};
+
 export default Summary;
